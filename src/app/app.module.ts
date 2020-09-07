0f6e7b6ad1491a9ae59f8e2460111e7866208784
Sara Álvarez Turrión <sara_sat93@hotmail.com>
2020-09-07 14:11:59 +0200

    hasta video 3.4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { DirectivaComponent } from './directiva/directiva.component';
+import { ClientesComponent } from './clientes/clientes.component';
+import {ClienteService } from './clientes/cliente.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    DirectivaComponent,
+    ClientesComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [ClienteService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/clientes/clientes.json.ts
@@ -0,0 +1,10 @@
+import {Cliente} from './cliente';
+
+export const CLIENTES: Cliente[] = [
+    {id:1, nombre:'Sara', apellido:'Álvarez', email:'sara_sat93@hotmail.com',createAt:'2020-08-07'},
+    {id:2, nombre:'Ana', apellido:'Esteban', email:'ana@hotmail.com',createAt:'2020-09-08'},
+    {id:3, nombre:'Tomás', apellido:'Crespo', email:'tom@hotmail.com',createAt:'2020-10-09'},
+    {id:4, nombre:'Fredes', apellido:'Turrión', email:'fredi@hotmail.com',createAt:'2020-11-010'},
+    {id:5, nombre:'Juanjo', apellido:'Álvarez', email:'juanji@hotmail.com',createAt:'2020-12-011'},
+
+  ]
