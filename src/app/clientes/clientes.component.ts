7c3a8d05b2538c8f4329d6d20645767209259740
Sara Álvarez Turrión <sara_sat93@hotmail.com>
2020-09-25 14:19:01 +0200

    paginacion con rango

--- src/app/clientes/clientes.component.ts
+++ src/app/clientes/clientes.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {Cliente} from './cliente';
 import { ClienteService } from './cliente.service';
-import Swal from 'sweetalert2'
+import Swal from 'sweetalert2';
+import {ActivatedRoute} from '@angular/router'
 
 @Component({
   selector: 'app-clientes',
@@ -10,14 +11,26 @@ import Swal from 'sweetalert2'
 export class ClientesComponent implements OnInit {
 
   clientes: Cliente[];
+  paginator: any;
 
-  constructor(private clienteService: ClienteService) { }
+  constructor(private clienteService: ClienteService, private activatedRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
-    let page = 0;
-    this.clienteService.getClientes(page).subscribe(
-      response => this.clientes = response.content as Cliente[]
-    );
+    
+    this.activatedRoute.paramMap.subscribe(params => {
+      let page:number = +params.get('page');
+
+      if(!page){
+        page=0;
+      }
+
+      this.clienteService.getClientes(page).subscribe(
+        response => {
+          this.clientes = response.content as Cliente[];
+          this.paginator = response
+        }
+      );
+    })
   }
 
   delete(cliente:Cliente):void{
---
+++ src/app/paginator/paginator.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit, Input, OnChanges } from '@angular/core';
+
+@Component({
+  selector: 'app-paginator',
+  templateUrl: './paginator.component.html'
+})
+export class PaginatorComponent implements OnInit, OnChanges {
+
+  @Input() paginator: any;
+
+  paginas: number[];
+
+  desde: number;
+  hasta: number;
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+    
+  }
+  ngOnChanges(): void{
+    
+    this.desde=Math.min(Math.max(1,this.paginator.number-4), this.paginator.totalPages-5);
+    this.hasta=Math.max(Math.min(this.paginator.totalPages, this.paginator.number+4),6);
+
+    if(this.paginator.totalPages>5){
+      
+      this.paginas = new Array(this.hasta - this.desde +1).fill(0).map((_valor, indice)=> indice + this.desde);
+
+    }else{
+      this.paginas = new Array(this.paginator.totalPages).fill(0).map((_valor, indice)=> indice+1);
+    }
+  }
+
+}
